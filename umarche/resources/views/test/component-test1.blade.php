67fc98e66f23ff2531dcda9cf306a050b30b44e9
Matsuoka0711 <taiki1544.0711@gmail.com>
2024-09-23 08:56:25 +0900

    属性バック

--- umarche/resources/views/test/component-test1.blade.php
+++ umarche/resources/views/test/component-test1.blade.php
@@ -7,4 +7,5 @@
   {{-- 変数を渡す場合は「:」をつける --}}
   <x-tests.card title="タイトルです1" content="コンテンツ1" :message="$message" />
   <x-tests.card title="タイトルです2"/>
+  <x-tests.card title="CSSを変更したい" class="bg-red-300" />
 </x-tests.app>
