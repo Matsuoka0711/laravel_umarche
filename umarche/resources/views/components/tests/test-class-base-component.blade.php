871a95f3b781caab3296e7c6c8be642670b45db3
Matsuoka0711 <taiki1544.0711@gmail.com>
2024-09-23 09:26:42 +0900

    classBase

---
+++ umarche/resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネント
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- Be present above all else. - Naval Ravikant -->
+</div>
--- umarche/resources/views/test/component-test2.blade.php
+++ umarche/resources/views/test/component-test2.blade.php
@@ -3,4 +3,7 @@
     ヘッダーです。
   </x-slot>
   コンポーネントテスト2
+  <x-test-class-base classBaseMessage="メッセージです"/>
+  <div class="md-4"></div>
+  <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています。" />
 </x-tests.app>
